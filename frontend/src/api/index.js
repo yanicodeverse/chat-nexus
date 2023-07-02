56719af74ce5cbb706f22aab8dd26c34c7dedbdb
rakshyak-98 <rakshak1998@gmail.com>
2023-07-03 01:19:03 +0530

    resolve: not reading from env.local file

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,13 +1,16 @@
 import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({debug: true, path: path.resolve(process.cwd(), ".env.local")});
 
 const socket = new Server(http.createServer(), {
 	cors: {
 		origin: `${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`,
 	},
 });
+
+console.log(process.env.API_PORT)
 
 socket.listen(process.env.API_PORT);
