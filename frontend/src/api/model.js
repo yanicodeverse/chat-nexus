7da2e6cff80273e3577b0fc1ef3554e3ad8504fa
rakshyak-98 <rakshak1998@gmail.com>
2023-07-08 10:49:51 +0530

    new: create required schemas

--- frontend/src/api/model.js
+++ frontend/src/api/model.js
@@ -1,19 +1,31 @@
-import mongoose from "mongoose"
+import mongoose, { SchemaTypes } from "mongoose"
 
 const ContactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: true,
-        minLength: 3,
-        maxLength: 60
-    },
-    contactID: {
-        type: String,
-        unique: true,
-        required: true
-    },
-
+    name: {type: String, unique: true, required: true, minLength: 3, maxLength: 60},
+    contactID: {type: String, unique: true, required: true},
 })
+
+const UserSchema = {
+    user_id: String,
+    username: String,
+    created_at: Date,
+}
+
+const ConversationsSchema = {
+    create_at: SchemaTypes
+}
+
+const ParticipantSchema = {
+    user_id: {type: SchemaTypes.ObjectId, ref: "User"},
+    conversation_id: {type: mongoose.SchemaTypes.ObjectId, ref: "Conversation"},
+    joined_at: Date
+}
+
+const MessageSchema = {
+    conversation_id: {type:SchemaTypes.ObjectId, ref: "Conversation"},
+    sender_id: {type: SchemaTypes.ObjectId, ref: "User"},
+    content: String,
+    sent_at: Date
+}
 
 export const Contact = mongoose.model('Contact', ContactSchema)
